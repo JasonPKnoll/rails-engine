15bf641dc9bb20916cac287b02039a4707ca7097
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-07 02:03:10 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index]
+      resources :customers, only: [:index, :show]
+      resources :merchants, only: [:index, :show]
     end
   end
 end
