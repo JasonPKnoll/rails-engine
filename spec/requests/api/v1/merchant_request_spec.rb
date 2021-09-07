5d63da0aaa375746b82669d52e8db07d3f091744
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-07 02:04:06 -0700

    Update merchant request spec to for pagination

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 describe "Merchant API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 3)
+  it "sends a subset of merchants based on pagination" do
+    create_list(:merchant, 40)
 
-    get 'api/v1/merchants'
+    get api_v1_merchants_path, params: { limit: 20, offset: 1 }
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(merchants[:data].count).to eq(20)
+  end
+
+  it "can grab a merchant by that merchants id" do
+    id = create(:merchant).id
+
+    get api_v1_merchant_path(id)
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["id"]).to eq(id)
   end
 end
