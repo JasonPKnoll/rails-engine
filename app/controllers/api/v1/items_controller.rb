7d6453166d1c0b3ee38382884e91345211298640
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-08 01:11:17 -0700

    Add items index method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::ItemsController < ApplicationController
 
-
+  def index
+    items = Item.limit(per_page).offset(page)
+    render json: ItemSerializer.new(items)
+  end
 
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     merchants = Merchant.limit(per_page).offset(page)
     render json: MerchantSerializer.new(merchants)
-    # render json: merchants
   end
 
   def show
