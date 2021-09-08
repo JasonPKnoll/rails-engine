cfcd8876fde427c3a8ee7022ab503931c42a56ce
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-08 01:52:17 -0700

    Add method for find item by id

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,4 +5,13 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(items)
   end
 
+  def show
+    item = Item.find_by_id(params[:id])
+    if item.equal?(nil)
+      record_not_found
+    else
+      render json: ItemSerializer.new(item)
+    end
+  end
+
 end
