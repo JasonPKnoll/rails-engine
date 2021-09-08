aa48c4e5747a3de287973988ef2fc4a9e2967ddb
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-08 00:03:57 -0700

    Refactor page and per_page into application controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -9,14 +9,4 @@ class Api::V1::MerchantsController < ApplicationController
     merchant = Merchant.find_by_id(params[:id])
     render json: MerchantSerializer.new(merchant)
   end
-
-  def page
-    min_page_number = 1
-    ([params.fetch(:page, min_page_number).to_i, min_page_number].max - min_page_number) * per_page
-  end
-
-  def per_page
-    default = 20
-    [ params.fetch(:per_page, default).to_i, default ].max  #fetch allows us to manually a limit if fetch request comes back as NIL
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+  def page
+    min_page_number = 1
+    ([params.fetch(:page, min_page_number).to_i, min_page_number].max - min_page_number) * per_page
+  end
+
+  def per_page
+    default = 20
+    [ params.fetch(:per_page, default).to_i, default ].max
+  end
+
 end
