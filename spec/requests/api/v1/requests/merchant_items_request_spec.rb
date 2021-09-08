e688718677aec5085d610fa72d2e60f98a20ca34
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-07 23:59:05 -0700

    Add test for 404 error in merchant items

---
+++ spec/requests/api/v1/requests/merchant_items_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Merchant Items API" do
+  it "grabs all merchant items" do
+    merchant_id = create(:merchant).id
+    merchant_id_2 = create(:merchant).id
+    create_list(:item, 3, merchant_id: merchant_id)
+    create_list(:item, 1, merchant_id: merchant_id_2)
+
+    get "/api/v1/merchants/#{merchant_id}/items"
+
+    merchant_items = JSON.parse(response.body, symbolize_names: true)
+    expect(merchant_items[:data].count).to eq(3)
+  end
+
+  it "returns 404 when no merchant found" do
+    merchant_id = 1
+
+    get "/api/v1/merchants/#{merchant_id}/items"
+
+    expect(response.body).to eq(404)
+  end
+end
