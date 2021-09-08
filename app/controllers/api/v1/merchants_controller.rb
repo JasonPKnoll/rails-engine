093593a3cf9dcc315502f0c013b89932caf3de77
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-07 19:59:29 -0700

    Add page and per_page methods to merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.limit(params[:limit]).offset(params[:offset])
+    merchants = Merchant.limit(per_page).offset(page)
     render json: MerchantSerializer.new(merchants)
     # render json: merchants
   end
 
   def show
     merchant = Merchant.find_by_id(params[:id])
-    render json: MerchantSerializer.format_merchants(merchant)
+    render json: MerchantSerializer.new(merchant)
   end
 
-  def grab_merchants(page, per_each_page)
-    require "pry"; binding.pry
-    Merchant.all
+  def page
+    min_page_number = 1
+    ([params.fetch(:page, min_page_number).to_i, min_page_number].max - min_page_number) * per_page
+  end
+
+  def per_page
+    default = 20
+    [ params.fetch(:per_page, default).to_i, default ].max  #fetch allows us to manually a limit if fetch request comes back as NIL
   end
 end
