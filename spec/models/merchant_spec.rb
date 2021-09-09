5672e433582c876969959798ed66dcde721a1216
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 02:17:08 -0700

    Add find by merchant name method test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe "class methods" do
+    it "find_by_name and list in order grabbing the first one" do
+      merchant_1 = create(:merchant, name: "Biggie Smalls")
+      merchant_2 = create(:merchant, name: "Big Chungus")
+      merchant_3 = create(:merchant, name: "Mike")
 
+      expect(Merchant.find_by_name("Big")).to eq(merchant_2)
+    end
+  end
 end
