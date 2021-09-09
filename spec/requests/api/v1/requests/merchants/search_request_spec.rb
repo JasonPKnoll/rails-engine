e97835c1cff8c7228f1f1ec503f7beb4236229b3
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 02:16:37 -0700

    Add merchant search spec tests

---
+++ spec/requests/api/v1/requests/merchants/search_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Merchant search API" do
+  before(:each, :with_merchants) do
+    @merchant = create(:merchant, name: "Find Me Please")
+    create_list(:merchant, 10)
+  end
+  describe "::Find by name" do
+    describe "::Happy path" do
+      it "finds the merchant by name", :with_merchants do
+        get "/api/v1/merchants/find?name=find" #, params: { search: "Find" }
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(merchants[:data][:attributes][:name]).to eq(@merchant.name)
+      end
+    end
+    describe "::Sad Path" do
+      it "gives error when finding no merchant", :with_merchants do
+        get "/api/v1/merchants/find?name=lovesick"
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+end
