591d934cc1ed9f9b1643c8f082be403650204a01
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 02:17:46 -0700

    Add find by name model to merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_by_name(search)
+    merchants = Merchant.arel_table
+    Merchant.where(merchants[:name].matches("%#{search}%")).order(:name).first
+
+    # where("name.downcase ILIKE ?", "#{name}%.").order(:name)
+  end
 end
