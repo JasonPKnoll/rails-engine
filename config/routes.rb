87e1e6f9e2e061e9ee9b466e2a223a67d936189b
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 12:20:25 -0700

    Update routes for items search function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find', to: 'merchants/search#find'
+      get 'items/find_all', to: 'items/search#find_all'
 
       resources :customers, only: [:index, :show]
       resources :merchants, only: [:index, :show] do
