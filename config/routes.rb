6d67c184404d8cf2f7fb03ea1e622191970b075b
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 02:17:26 -0700

    Updated routes for merchant search

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/homepage'
   namespace :api do
     namespace :v1 do
+      get 'merchants/find', to: 'merchants/search#find'
+
       resources :customers, only: [:index, :show]
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchant_items#index'
         # resources :items, as: :merchant_items, only: :index
       end
+
       resources :items, except: [:new, :edit] do
         get "/merchant", to: 'items#find_merchant'
       end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-
+  describe "class methods" do
+  end
 end
