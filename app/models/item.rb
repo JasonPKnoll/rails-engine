4b0ee271edcd9cd8216a142a083e7cb17a62edeb
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-09 18:38:47 -0700

    Expand table relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
 
   def self.find_all_by_name(search)
     items = Item.arel_table
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,18 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.find_by_name(search)
     merchants = Merchant.arel_table
     Merchant.where(merchants[:name].matches("%#{search}%")).order(:name).first
 
     # where("name.downcase ILIKE ?", "#{name}%.").order(:name).first
+  end
+
+  def find_merchant_rev
+    # require "pry"; binding.pry
   end
 end
