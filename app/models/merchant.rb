132f199ad0fca4c08e3f51ada02b637000536e93
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-10 02:22:26 -0700

    Add revenue models to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,7 +12,20 @@ class Merchant < ApplicationRecord
     # where("name.downcase ILIKE ?", "#{name}%.").order(:name).first
   end
 
+  def self.find_most_revenue(total)
+    Merchant.joins(items: [invoices: :transactions])
+            .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+            .where("transactions.result =?", "success")
+            .where("invoices.status = ?", "shipped")
+            .group("merchants.id")
+            .order("revenue DESC")
+            .limit(total)
+  end
+
   def find_merchant_rev
-    # require "pry"; binding.pry
+    items.joins(invoices: :transactions)
+         .where("invoices.status = ?", "shipped")
+         .where("transactions.result = ?", "success")
+         .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 end
