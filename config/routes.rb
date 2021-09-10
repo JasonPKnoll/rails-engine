681c10dee1b9ef9cadbfa938b0fa74a5ebeb39df
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-10 03:26:40 -0700

    Add route for items revenue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'welcome/homepage'
   namespace :api do
     namespace :v1 do
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
       get 'revenue/merchants/:id', to: 'merchants/revenue#merchant_total_revenue'
       get 'revenue/merchants', to: 'merchants/revenue#most_revenue_merchants'
       get 'items/find_all', to: 'items/search#find_all'
+      get 'revenue/items', to: 'items/revenue#most_revenue_items'
+
 
       resources :customers, only: [:index, :show]
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchant_items#index'
-        # resources :items, as: :merchant_items, only: :index
       end
 
       resources :items, except: [:new, :edit] do
